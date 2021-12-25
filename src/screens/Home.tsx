cc1278164ae27378b4408890cde632b007d94cbb
Abdur Rakib <rakib@brainstation-23.com>
2021-12-25 13:28:36 +0600

    Store selected mood to a list

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {MoodPicker} from '../components/MoodPicker';
+import {MoodOptionType, MoodOptionWithTimestamp} from '../types';
 
 export const Home: React.FC = () => {
+  const [moodList, setMoodList] = React.useState<MoodOptionWithTimestamp[]>([]);
+  const handleSelectMood = React.useCallback((mood: MoodOptionType) => {
+    setMoodList(current => [...current, {mood, timestamp: Date.now()}]);
+  }, []);
+  console.log({moodList});
   return (
     <View style={styles.container}>
-      <MoodPicker />
+      <MoodPicker onSelect={handleSelectMood} />
     </View>
   );
 };
